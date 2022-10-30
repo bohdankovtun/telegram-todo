b13a759fc5c07b880b4bde4974ead0bf4a5e9c3f
Bohdan Kovtun <alexnag990@gmail.com>
2022-10-30 15:25:40 +0200

    Remove word

--- src/App.tsx
+++ src/App.tsx
@@ -8,10 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload. Create your first React App!
+          Edit <code>src/App.tsx</code> and save to reload. Create first React App!
         </p>
         <a
-          className="App"
+          className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
